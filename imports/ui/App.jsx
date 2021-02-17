adde1dc685f7809eb2b6e8784f8d13f17589dd6b
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2021-02-17 18:22:29 +0100

    fix packages & better UI

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,7 +3,7 @@ import FileUpload from './FileUpload.jsx';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
+    <h1>Try to upload a file</h1>
     <FileUpload />
   </div>
 );
--- imports/ui/FileUpload.jsx
+++ imports/ui/FileUpload.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ipfsClient from "ipfs-http-client";
 
 const apiUrl = "https://ipfs.infura.io:5001";
@@ -14,18 +14,25 @@ async function onUpload(file) {
 }
 
 function FileUpload() {
+  const [uploading, setUploading] = useState(false);
+
   return (
-    <input
-      type="file"
-      label="Add file"
-      onChange={(e) => {
-        onUpload(e.target.files[0])
-          .then(hash => {
-            alert(`Success! File hash: ${hash}`)
-          })
-          .catch(e => alert(e.message))
-      }}
-    />
+    <>
+      <input
+        type="file"
+        label="Add file"
+        onChange={(e) => {
+          setUploading(true);
+          onUpload(e.target.files[0])
+            .then(hash => {
+              alert(`Success! File hash: ${hash}`)
+            })
+            .catch(e => alert(e.message))
+            .finally(() => setUploading(false))
+        }}
+      />
+      {uploading && <div>Uploading...</div>}
+    </>
   );
 }
 
